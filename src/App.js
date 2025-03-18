274c90cadbe0c6af7701a3b80850a0c3bbc543ae
Victoria Rodríguez <vrg_rodriguez@hotmail.com>
2025-03-18 20:00:54 +0100

    Subimos el proyecto de atla a github

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import imgGaang from './img/gaang.png'
+import Characters from './Components/Characters';
 
 function App() {
+
+  const [characters, setCharacters] = useState(null);
+  const gaangApi = async () => {
+    const api = await fetch("https://last-airbender-api.fly.dev/api/v1/characters/random?count=21");
+    const characterApi = await api.json();
+  
+    setCharacters(characterApi);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="title">Avatar: The Series</h1>
+        
+        {characters ? ( //Le decimos que si characters contiene algo nos cargue el componente de Characters y cuando no, nos cargue la página de inico
+          <Characters characters ={characters}/>
+        ) :(
+          //Lo encapsulamos en un fragment para que no nos de error el JSX
+          <>
+          <img src={imgGaang} alt="gaang" className="img-home"></img>
+        <button onClick={gaangApi} className="btn-search">Buscar personajes</button></>
+        ) }
+        
+        
       </header>
     </div>
   );
---
+++ src/Components/Characters.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export default function Characters(props) {
+    const { characters } = props;
+
+    const handleBackHome = () => {
+        window.location.href = '/';  // Redirige a la página principal
+    };
+
+    return (
+        <div className="characters">
+            <h1>Personajes</h1>
+            <span className="back-home" onClick={handleBackHome}>Volver al Inicio</span> {/* Al hacer click se redirige */}
+            <div className="container-characters">
+                {characters.map((character, index) => (
+                    <div className="character-container" key={index}>
+                        <div>
+                            <img src={character.photoUrl} alt={character.name}></img>
+                        </div>
+
+                        <div>
+                            <h3>{character.name}</h3>
+                            <p>Gender: <span>{character.gender ? character.gender : "Unknown"}</span></p>
+                            {character.hair && <p>Hair: <span>{character.hair}</span></p>}
+                            {character.eye && <p>Eye color: <span>{character.eye}</span></p>}
+                            {character.skin && (
+                                <p>{character.skin === "Fur" ? "Skin" : "Skin tone"}: {character.skin}</p>
+                            )}
+                            <p>Bending/Weapon: <span>{character.weapon ? character.weapon : "None"}</span></p>
+                            <p>Profession: <span>{character.profession ? character.profession : "None"}</span></p>
+                            {character.affiliation && <p>Affiliated with {character.affiliation}</p>}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
